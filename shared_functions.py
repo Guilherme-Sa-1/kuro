792373bd9e23bec1dffed73fec9a06a4108b6317
Guilherme Sá <137445677+Guilherme-Sa-1@users.noreply.github.com>
2025-02-18 15:33:15 -0300

    Update shared_functions.py

--- shared_functions.py
+++ shared_functions.py
@@ -2,7 +2,5 @@ from features.news import get_news, format_news
 from features.weather import get_weather, format_weather
 
 def greet_user(username):
-    """
-    A shared function to greet the user.
-    """
+ 
     return f"Hello, {username}! Welcome to Kuro."
